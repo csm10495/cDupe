31be029acdaad0602edd4ecc9c5b08581425b0a9
csm10495 <csm10495@gmail.com>
2014-06-14 03:31:00 -0400

    inital git commit

---
+++ cDupe/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace cDupe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripResizeToScale = new System.Windows.Forms.ToolStripMenuItem();
+            this.resizeMirrorRegionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.beginMirrorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshSpeedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ComboBoxRefreshRate = new System.Windows.Forms.ToolStripComboBox();
+            this.AboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.AutoSize = false;
+            this.menuStrip1.BackColor = System.Drawing.Color.Transparent;
+            this.menuStrip1.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(651, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripResizeToScale,
+            this.resizeMirrorRegionToolStripMenuItem,
+            this.beginMirrorToolStripMenuItem,
+            this.refreshSpeedToolStripMenuItem,
+            this.AboutToolStripMenuItem,
+            this.exitToolStripMenuItem1});
+            this.fileToolStripMenuItem.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // ToolStripResizeToScale
+            // 
+            this.ToolStripResizeToScale.Checked = true;
+            this.ToolStripResizeToScale.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ToolStripResizeToScale.Name = "ToolStripResizeToScale";
+            this.ToolStripResizeToScale.Size = new System.Drawing.Size(199, 22);
+            this.ToolStripResizeToScale.Text = "Resize to Scale";
+            this.ToolStripResizeToScale.Click += new System.EventHandler(this.ToolStripResizeToScale_Click);
+            // 
+            // resizeMirrorRegionToolStripMenuItem
+            // 
+            this.resizeMirrorRegionToolStripMenuItem.Name = "resizeMirrorRegionToolStripMenuItem";
+            this.resizeMirrorRegionToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.resizeMirrorRegionToolStripMenuItem.Text = "Resize Mirror Region";
+            this.resizeMirrorRegionToolStripMenuItem.Click += new System.EventHandler(this.resizeMirrorRegionToolStripMenuItem_Click);
+            // 
+            // beginMirrorToolStripMenuItem
+            // 
+            this.beginMirrorToolStripMenuItem.Name = "beginMirrorToolStripMenuItem";
+            this.beginMirrorToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.beginMirrorToolStripMenuItem.Text = "Start Mirror";
+            this.beginMirrorToolStripMenuItem.Click += new System.EventHandler(this.beginMirrorToolStripMenuItem_Click);
+            // 
+            // refreshSpeedToolStripMenuItem
+            // 
+            this.refreshSpeedToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ComboBoxRefreshRate,
+            this.toolStripSeparator1,
+            this.toolStripMenuItem1});
+            this.refreshSpeedToolStripMenuItem.Name = "refreshSpeedToolStripMenuItem";
+            this.refreshSpeedToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.refreshSpeedToolStripMenuItem.Text = "Refresh Rate";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(186, 6);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Enabled = false;
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(189, 22);
+            this.toolStripMenuItem1.Text = "Refresh per second";
+            // 
+            // ComboBoxRefreshRate
+            // 
+            this.ComboBoxRefreshRate.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append;
+            this.ComboBoxRefreshRate.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.ComboBoxRefreshRate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBoxRefreshRate.Name = "ComboBoxRefreshRate";
+            this.ComboBoxRefreshRate.Size = new System.Drawing.Size(121, 23);
+            this.ComboBoxRefreshRate.SelectedIndexChanged += new System.EventHandler(this.ComboBoxRefreshRate_SelectedIndexChanged);
+            // 
+            // AboutToolStripMenuItem
+            // 
+            this.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem";
+            this.AboutToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.AboutToolStripMenuItem.Text = "About";
+            this.AboutToolStripMenuItem.Click += new System.EventHandler(this.AboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(199, 22);
+            this.exitToolStripMenuItem1.Text = "Exit";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(651, 493);
+            this.panel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 517);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "cDupe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem AboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem beginMirrorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshSpeedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripResizeToScale;
+        private System.Windows.Forms.ToolStripMenuItem resizeMirrorRegionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox ComboBoxRefreshRate;
+    }
+}
+
---
+++ cDupe/Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+
+namespace cDupe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// False: not currently mirroring
+        /// True: currently mirroring
+        /// </summary>
+        bool mirroring = false;
+
+        /// <summary>
+        /// width of duplication zone
+        /// </summary>
+        int x_size = 255;
+
+        /// <summary>
+        /// height of duplication zone
+        /// </summary>
+        int y_size = 230;
+
+
+        /// <summary>
+        /// x based point location of duplication zone
+        /// </summary>
+        int x_loc = 0;
+
+        /// <summary>
+        /// y based point location of duplication zone
+        /// </summary>
+        int y_loc = 0;
+
+        /// <summary>
+        /// bitmap of duplication zone
+        /// </summary>
+        Bitmap img = null;
+
+        /// <summary>
+        /// Graphics to be used to grab duplication zone
+        /// </summary>
+        Graphics graphics;
+
+        /// <summary>
+        /// form loading method
+        /// grab settings from Properties.Settings.Default
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //set resize toolstrip check to saved setting
+            ToolStripResizeToScale.Checked = Properties.Settings.Default.resize_to_ratio;
+
+            //get other settings
+            x_size = Properties.Settings.Default.x_size;
+            y_size = Properties.Settings.Default.y_size;
+            x_loc = Properties.Settings.Default.x_loc;
+            y_loc = Properties.Settings.Default.y_loc;
+
+            //set combobox values to be from 1 to 120
+            ComboBoxRefreshRate.Items.Clear();
+            for (int i = 1; i != 251; i++)
+            {
+                ComboBoxRefreshRate.Items.Add(i);
+            }
+            //grab value from settings
+            ComboBoxRefreshRate.Text = Properties.Settings.Default.refresh_rate.ToString();
+
+            //if ratio is enforced, enforce it
+            if (ToolStripResizeToScale.Checked)
+            {
+                scaleToDuplicationZoneRatio();
+            }
+        }
+
+        /// <summary>
+        /// called on every timer tick, to refresh the background image
+        /// </summary>
+        private void refreshImage()
+        {
+            //manual memory management
+            if (this.BackgroundImage != null)
+            {
+                this.BackgroundImage.Dispose();
+                this.BackgroundImage = null;
+            }
+
+            //hopefully unnecessary try-catch
+            try
+            {
+                //creates image of duplication zone and sets it to be the background image
+                img = new Bitmap(x_size, y_size);
+                graphics = Graphics.FromImage(img as Image);
+                graphics.CopyFromScreen(x_loc, y_loc, 0, 0, new Size(x_size, y_size));
+                this.BackgroundImage = (Image)img;
+            }
+            catch (Exception ex)   //I can't think of a reason for exception, but we shall see
+            {
+                MessageBox.Show(ex.ToString()); ;
+            }
+
+            //force background to stay stretched
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+
+            //manual memory management
+            graphics.Dispose();
+
+        }
+
+        /// <summary>
+        /// called on every timer tick
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            refreshImage();
+        }
+
+        /// <summary>
+        /// exit menu item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// begin or stop mirroring of duplication zone
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void beginMirrorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //if we are not mirroring, begin mirroring
+            if (!mirroring)
+            {
+                //if ratio is enforced, enforce it
+                if (ToolStripResizeToScale.Checked)
+                {
+                    scaleToDuplicationZoneRatio();
+                }
+
+                timer1.Start();
+                beginMirrorToolStripMenuItem.Text = "Stop Mirror";
+                mirroring = true;
+                panel1.Visible = false;
+                menuStrip1.BackColor = Color.Transparent;
+            }
+            //if we are mirroring, stop mirroring and put a panel in front of the last frame to show it is done
+            else
+            {
+                timer1.Stop();
+                beginMirrorToolStripMenuItem.Text = "Start Mirror";
+                mirroring = false;
+                panel1.Visible = true;
+                menuStrip1.BackColor = SystemColors.Control;
+            }
+        }
+
+        /// <summary>
+        /// ensures that the form only reszes to proper ratios
+        /// that respect the ratio of the duplication zone
+        /// </summary>
+        private void scaleToDuplicationZoneRatio()
+        {
+            //use ratio to compute height
+            double ratio = Convert.ToDouble(y_size) / Convert.ToDouble(x_size);
+            int new_height = Convert.ToInt16(Convert.ToDouble(this.Width) * ratio);
+            this.Height = new_height;
+        }
+
+
+        /// <summary>
+        /// resize form to proper ratio on resize if needed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (ToolStripResizeToScale.Checked == true)
+                scaleToDuplicationZoneRatio();
+        }
+
+        /// <summary>
+        /// opens second form to setup duplication zone
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void resizeMirrorRegionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Location = new Point(Properties.Settings.Default.x_loc, Properties.Settings.Default.y_loc);
+            f2.Size = new Size(Properties.Settings.Default.x_size, Properties.Settings.Default.y_size);
+            f2.Show();
+
+            //add event handler
+            f2.FormClosing += new FormClosingEventHandler(F2_Closing);
+        }
+
+
+        /// <summary>
+        /// When the setup form is closing, this is called
+        /// it grabs location/size data from the form and saves it
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void F2_Closing(object sender, FormClosingEventArgs e)
+        {
+            x_size = ((Form)sender).Width;
+            y_size = ((Form)sender).Height;
+            x_loc = ((Form)sender).Location.X;
+            y_loc = ((Form)sender).Location.Y;
+
+            //save settings to my.settings equivalent
+            Properties.Settings.Default.x_size = x_size;
+            Properties.Settings.Default.y_size = y_size;
+            Properties.Settings.Default.x_loc = x_loc;
+            Properties.Settings.Default.y_loc = y_loc;
+            Properties.Settings.Default.Save();
+
+            //if ratio is enforced, enforce it
+            if (ToolStripResizeToScale.Checked)
+            {
+                scaleToDuplicationZoneRatio();
+            }
+        }
+
+
+        //does nothing with the object
+        //use to remove warnings about not using variables
+        private void do_nothing_with(object o)
+        {
+            //does nothing
+        }
+
+        /// <summary>
+        /// called when user changes refresh rate
+        /// must change value in settings
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ComboBoxRefreshRate_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.refresh_rate = Convert.ToInt16(ComboBoxRefreshRate.Text.ToString());
+            Properties.Settings.Default.Save();
+
+            timer1.Interval = (1000) / Convert.ToInt16(ComboBoxRefreshRate.Text);
+
+        }
+
+
+        /// <summary>
+        /// checkbox to signify if the main form should scale to the same ratio as the duplication zone
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ToolStripResizeToScale_Click(object sender, EventArgs e)
+        {
+            //boolean flip
+            ToolStripResizeToScale.Checked = !ToolStripResizeToScale.Checked;
+
+            //write state to settings
+            Properties.Settings.Default.resize_to_ratio = ToolStripResizeToScale.Checked;
+            Properties.Settings.Default.Save();
+        }
+
+
+        /// <summary>
+        /// About messagebox
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("cDupe allows you to magnify a region of a screen. It was developed as a simple solution to enlarge the World of Tanks minimap onto a second monitor. Enjoy!", "MIT License - csm10495");
+        }
+    }
+}
